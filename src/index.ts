c35b7067c478ad6539ce2f73d92a731e6bad0949
Zachary Albia <zach.albia@gmail.com>
2025-10-07 20:52:24 +0800

    feat: error handling example

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-import { Console, Effect } from "effect";
+import { Effect } from "effect";
+import type { UnknownException } from "effect/Cause";
 
-const main = Console.log("Hello world");
+const fetchRequest: Effect.Effect<Response, UnknownException> = Effect.promise(() =>
+  fetch("https://pokeapi.co/api/v2/pokemon/garchomp/")
+);
 
-Effect.runSync(main);
+const jsonResponse = (response: Response) =>
+  Effect.promise(() => response.json());
+
+const main = Effect.flatMap(fetchRequest, jsonResponse);
+
+Effect.runPromise(main);
