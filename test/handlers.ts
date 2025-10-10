5bda89a5d8c8c305d4ac6474518a1ab755a1ed54
Zachary Albia <zach.albia@gmail.com>
2025-10-11 00:19:03 +0800

    test: with services

---
+++ test/handlers.ts
@@ -0,0 +1,16 @@
+import { HttpResponse, http } from "msw";
+import type { Pokemon } from "../src/schemas.js";
+
+const mockPokemon: Pokemon = {
+  id: 1,
+  height: 10,
+  weight: 10,
+  order: 1,
+  name: "myname",
+};
+
+export const handlers = [
+  http.get("http://localhost:3000/api/v2/pokemon/*", () => {
+    return HttpResponse.json(mockPokemon);
+  }),
+];
---
+++ test/index.test.ts
@@ -0,0 +1,33 @@
+import { afterAll, afterEach, beforeAll, expect, it } from "vitest";
+import { server } from "../test/node.js";
+import { ConfigProvider, Effect, Layer } from "effect";
+import { PokeApi } from "../src/PokeApi.js";
+
+const TestConfigProvider = ConfigProvider.fromMap(
+  new Map([["BASE_URL", "http://localhost:3000"]])
+);
+
+const ConfigProviderLayer = Layer.setConfigProvider(TestConfigProvider);
+const MainLayer = PokeApi.Default.pipe(Layer.provide(ConfigProviderLayer));
+
+const program = Effect.gen(function* () {
+  const pokeApi = yield* PokeApi;
+  return yield* pokeApi.getPokemon;
+});
+
+const main = program.pipe(Effect.provide(MainLayer));
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+it("returns a valid pokemon", async () => {
+  const response = await Effect.runPromise(main);
+  expect(response).toEqual({
+    id: 1,
+    height: 10,
+    weight: 10,
+    order: 1,
+    name: "myname",
+  });
+});
