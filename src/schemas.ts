045baa6c917a537379070c3e833dbe8c0aab8c30
Zachary Albia <zach.albia@gmail.com>
2025-10-16 21:48:36 +0800

    feat: effectful codec example with transform fix for dm to cm

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,9 +1,10 @@
-import { Schema } from "effect";
+import { Effect, Schema } from "effect";
+import type { ParseError } from "effect/ParseResult";
 
 const HeightFormat = Schema.Number.pipe(
   Schema.transform(Schema.String, {
-    decode: from => `${from}cm`,
-    encode: to => Number(to.substring(0, to.length - 2)),
+    decode: from => `${from * 10}cm`,
+    encode: to => Number(to.substring(0, to.length - 2)) / 10,
   })
 );
 
@@ -14,3 +15,10 @@ export class Pokemon extends Schema.Class<Pokemon>("Pokemon")({
   height: HeightFormat,
   weight: Schema.Number,
 }) {}
+
+type Encoded = typeof HeightFormat.Encoded;
+type Decoded = typeof HeightFormat.Type;
+
+// effectful codec
+const heightFormat: Effect.Effect<string, ParseError> = Schema.decode(HeightFormat)(17.5);
+const backToNumber: Effect.Effect<number, ParseError> = Schema.encode(HeightFormat)("175cm");
